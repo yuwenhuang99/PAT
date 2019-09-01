e6e76354cafe03813326492b33efbb8098c2f1ac
Baron0319 <45302888+Baron0319@users.noreply.github.com>
2019-09-01 21:07:34 +0800

    Update 1001. A+B Format (20).cpp

--- Advanced Level__C++/1001. A+B Format (20).cpp
+++ Advanced Level__C++/1001. A+B Format (20).cpp
@@ -1,3 +1,18 @@
+1001 A+B Format (20 分)
+Calculate a+b and output the sum in standard format -- that is, the digits must be separated into groups of three by commas (unless there are less than four digits).
+Input
+Each input file contains one test case. Each case contains a pair of integers a and b where -1000000 <= a, b <= 1000000. The numbers are separated by a space.
+Output
+For each test case, you should output the sum of a and b in one line. The sum must be written in the standard format.
+Sample Input:
+-1000000 9
+Sample Output:
+-999,991
+/*
+可以利用to_string简化 不必在通过取余然后存入数组的方式
+关键是什么时候添逗号，很明显在(len-1-i)为3的倍数 也就是剩余的数是3的倍数，当然不能是剩余的不能是0
+就没什么难的了
+*/
 #include<cstdio>
 #include<string>
 using namespace std;
